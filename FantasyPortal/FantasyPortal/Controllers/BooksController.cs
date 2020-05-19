288e51e46bc1641f8f1edf9044dde523b4ba6fbf
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2020-05-19 10:06:15 +0300

    Add Books Controller

---
+++ FantasyPortal/FantasyPortal/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FantasyPortal.Controllers
+{
+  public class BooksController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Details(int id)
+    {
+      ViewData["Message"] = "Book" + id.ToString();
+
+      return View();
+    }
+  }
+}
